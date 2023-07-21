c9a8a6020e34164b24877aefaa01d5a0bea08847
GianmauroCoviello <gianmaurocoviello@gmail.com>
2023-07-21 15:01:00 +0200

    aggiunto style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,12 +10,21 @@
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
         <!-- Styles -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 
         
     </head>
     <body class="antialiased">
         <!-- riprendisamo i dati dell'array associativo sottofoRma di variabili php nel DOM -->
-        <h1>{{$title}}</h1>
-        <p>{{$Text}}</p>
+        <div class="container bg-danger">
+            <div class="row">
+                <div class="col-12 text-center">
+                    <h1>{{$title}}</h1>
+                    <p>{{$Text}}</p>
+                </div>
+            </div>
+
+        </div>
+        
     </body>
 </html>
